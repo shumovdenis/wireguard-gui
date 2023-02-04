af94357964335198f9706c7eedc7cdec8e5f1976
denis <denis-shumoff@yandex.ru>
2023-02-04 16:21:12 +0300

    add th delete button

--- src/main/java/com/shumovdenis/wireguardgui/controller/ThController.java
+++ src/main/java/com/shumovdenis/wireguardgui/controller/ThController.java
@@ -5,31 +5,44 @@ import com.shumovdenis.wireguardgui.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ThController {
 
     UserService userService;
-    UserRepository userRepository;
 
     @Value("${wg.address}")
     private String serverIP;
 
 
-    public ThController(UserService userService, UserRepository userRepository) {
+    public ThController(UserService userService) {
         this.userService = userService;
-        this.userRepository = userRepository;
     }
 
-
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("users", userService.getUsers());
         return "index";
     }
 
+    @PostMapping("/index")
+    public String addUser(@RequestParam("name") String username,
+                        @RequestParam("allowedIPs") String allowedIPs,
+                          Model model
+    ) {
+        model.addAttribute("name", username);
+        model.addAttribute("allowedIPs", allowedIPs);
+        userService.addUser(username, allowedIPs);
+        return "redirect:index";
+    }
+
+
+    @PostMapping("/index/delete")
+    public String deleteUser(@RequestParam("username") String username) {
+        userService.deleteUser(username);
+        return "redirect:/index";
+    }
 
 
 }
